3770d7519510a9a4cab07b784a9ec984f4d734ff
Filip-scripts <necula.info.pas@gmail.com>
2026-08-05 18:33:00 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, from Flask in Wasmer Edge 🚀"
-
+@app.route("/Diana")
+def treburi():
+    return "Diano, daca nu mai lucrezi, macar spune-mi. Si daca totusi vrei sa mai continui sa ma ajuti, eu cred co o voi lasa-o mai usor cu celallt proiect si voi face mai multe la asta. Pur si simplu, fara tine pare plictisitor."
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
